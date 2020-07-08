68638762a7d115c06ff7351d9f4e16390087f681
Winfidonarleyan <dowlandtop@yandex.com>
2020-07-08 12:38:40 +0700

    feat(Core/Gossip): add support multivendor
    Ref https://github.com/azerothcore/azerothcore-wotlk/pull/3172

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -124,6 +124,7 @@ WorldSession::WorldSession(uint32 id, WorldSocket* sock, AccountTypes sec, uint8
     recruiterId(recruiter),
     isRecruiter(isARecruiter),
     m_currentBankerGUID(0),
+    m_currentVendorEntry(0),
     timerGsSpam(0),
     _calendarEventCreationCooldown(0)
 {
