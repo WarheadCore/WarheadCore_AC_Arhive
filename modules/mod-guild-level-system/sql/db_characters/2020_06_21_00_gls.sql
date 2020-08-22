7fa36cd1c99290f0c57f990573977dba2e2f9632
Winfidonarleyan <dowlandtop@yandex.com>
2020-08-23 01:26:12 +0700

    add sql

--- modules/mod-guild-level-system/sql/db_characters/2020_06_21_00_gls.sql
+++ modules/mod-guild-level-system/sql/db_characters/2020_06_21_00_gls.sql
@@ -1,71 +1,51 @@
 -- ----------------------------
--- Table structure for gls_exp_for_level
+-- Table structure for gls_criteria
 -- ----------------------------
-DROP TABLE IF EXISTS `gls_exp_for_level`;
-CREATE TABLE `gls_exp_for_level`  (
-  `Level` int(10) NOT NULL DEFAULT 0,
-  `Exp` int(10) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`Level`) USING BTREE
+DROP TABLE IF EXISTS `gls_criteria`;
+CREATE TABLE `gls_criteria`  (
+  `CriteriaID` int(10) NOT NULL AUTO_INCREMENT,
+  `StageID` int(10) NOT NULL DEFAULT 1,
+  `ListItemID` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `ListItemCount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `MinPlayersCount` int(10) NOT NULL,
+  `Coef` float(10, 5) NOT NULL DEFAULT 1.00000,
+  `ListRewardSpells` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`CriteriaID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of gls_criteria
+-- ----------------------------
+INSERT INTO `gls_criteria` VALUES (1, 1, '37711, 40265, 37711', '100, 2, 2000', 1, 0.40000, '26662, 62555');
+INSERT INTO `gls_criteria` VALUES (2, 2, '37711', '100000', 1, 0.70000, '50494');
+
+-- ----------------------------
+-- Table structure for gls_criteria_progress
+-- ----------------------------
+DROP TABLE IF EXISTS `gls_criteria_progress`;
+CREATE TABLE `gls_criteria_progress`  (
+  `GuildID` int(10) NOT NULL DEFAULT 0,
+  `CriteriaID` int(10) NOT NULL DEFAULT 0,
+  `ItemCount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `SelectedSpell` int(10) NOT NULL,
+  `IsDone` tinyint(1) NOT NULL,
+  PRIMARY KEY (`GuildID`, `CriteriaID`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of gls_exp_for_level
+-- Records of gls_criteria_progress
 -- ----------------------------
-INSERT INTO `gls_exp_for_level` VALUES (1, 100);
-INSERT INTO `gls_exp_for_level` VALUES (2, 200);
-INSERT INTO `gls_exp_for_level` VALUES (3, 400);
-INSERT INTO `gls_exp_for_level` VALUES (4, 800);
-INSERT INTO `gls_exp_for_level` VALUES (5, 1000);
 
 -- ----------------------------
--- Table structure for gls_guild_level
+-- Table structure for gls_stages_progress
 -- ----------------------------
-DROP TABLE IF EXISTS `gls_guild_level`;
-CREATE TABLE `gls_guild_level`  (
-  `GuildID` int(10) NOT NULL DEFAULT 0,
-  `Level` int(10) NOT NULL DEFAULT 0,
-  `Exp` int(10) NOT NULL DEFAULT 0,
+DROP TABLE IF EXISTS `gls_stages_progress`;
+CREATE TABLE `gls_stages_progress`  (
+  `GuildID` int(10) NOT NULL DEFAULT 1,
+  `StageID` int(10) NOT NULL DEFAULT 1,
   PRIMARY KEY (`GuildID`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of gls_guild_level
+-- Records of gls_stages_progress
 -- ----------------------------
-INSERT INTO `gls_guild_level` SELECT `guildid`, 1, 0 FROM `guild`;
-
--- ----------------------------
--- Table structure for gls_guild_setting
--- ----------------------------
-DROP TABLE IF EXISTS `gls_guild_setting`;
-CREATE TABLE `gls_guild_setting`  (
-  `GuildID` int(10) NOT NULL DEFAULT 0,
-  `Flag` int(255) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`GuildID`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of gls_guild_setting
--- ----------------------------
-INSERT INTO `gls_guild_setting` SELECT `guildid`, 0 FROM `guild`;
-
--- ----------------------------
--- Table structure for gls_invested
--- ----------------------------
-DROP TABLE IF EXISTS `gls_invested`;
-CREATE TABLE `gls_invested`  (
-  `GuildID` int(10) NOT NULL,
-  `PlayerName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `InvestExp` int(10) NOT NULL,
-  `UnixDate` int(10) NOT NULL,
-  PRIMARY KEY (`GuildID`, `PlayerName`, `InvestExp`, `UnixDate`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for gls_reward_spell
--- ----------------------------
-DROP TABLE IF EXISTS `gls_reward_spell`;
-CREATE TABLE `gls_reward_spell`  (
-  `GuildLevel` int(10) NOT NULL DEFAULT 0,
-  `ListSpells` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`GuildLevel`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
