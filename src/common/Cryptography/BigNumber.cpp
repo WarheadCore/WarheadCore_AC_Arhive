1ee6a56772e2151b90f60873c75229391acf02b9
Viste <viste02@gmail.com>
2020-08-25 01:27:45 +0300

    Revert "Core/misc: ok no length"
    This reverts commit 5f15ce887ec41c902a040c13b3ea3fe67ee6b75f.

--- src/common/Cryptography/BigNumber.cpp
+++ src/common/Cryptography/BigNumber.cpp
@@ -173,7 +173,7 @@ std::unique_ptr<uint8[]> BigNumber::AsByteArray(int32 minSize, bool littleEndian
     if (length > numBytes)
         memset((void *)array, 0, length);
 
-    BN_bn2bin(_bn, (unsigned char*)array);
+    BN_bn2bin(_bn, array + (length - numBytes));
 
     // openssl's BN stores data internally in big endian format, reverse if little endian desired
     if (littleEndian)
