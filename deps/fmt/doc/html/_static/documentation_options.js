c31128d0ea517e45baf62d8e46d8f96097c771ed
Restyled.io <commits@restyled.io>
2020-09-11 02:16:53 +0000

    [Restyler] prettier

--- deps/fmt/doc/html/_static/documentation_options.js
+++ deps/fmt/doc/html/_static/documentation_options.js
@@ -1,10 +1,12 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.0.3',
-    LANGUAGE: 'None',
+    URL_ROOT: document
+        .getElementById("documentation_options")
+        .getAttribute("data-url_root"),
+    VERSION: "7.0.3",
+    LANGUAGE: "None",
     COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
+    FILE_SUFFIX: ".html",
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt',
+    SOURCELINK_SUFFIX: ".txt",
     NAVIGATION_WITH_KEYS: false,
 };
