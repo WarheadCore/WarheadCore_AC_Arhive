6332ef09509f05e0a9a4407af2047bd3cabd5e33
Restyled.io <commits@restyled.io>
2020-10-28 09:13:33 +0000

    [Restyler] shellharden

--- apps/ci/ci-core-compile.sh
+++ apps/ci/ci-core-compile.sh
@@ -6,10 +6,10 @@ echo "Compile core"
 export CCACHE_CPP2=true
 export CCACHE_MAXSIZE='500MB'
 export CCACHE_COMPRESS=1
-$MTHREADS=$(($(grep -c ^processor /proc/cpuinfo) + 2))
+"$MTHREADS=$(($(grep -c ^processor /proc/cpuinfo) + 2))"
 ccache -s
 cd build
 echo "> Using $MTHREADS threads"
-make -j $MTHREADS -k && make -j $MTHREADS install
+make -j "$MTHREADS" -k && make -j "$MTHREADS" install
 cd check_install/bin
 ccache -s
