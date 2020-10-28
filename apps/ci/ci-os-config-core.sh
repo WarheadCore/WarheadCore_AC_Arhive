a674628478aedbaf411eb94eb475fd4621aafc49
Restyled.io <commits@restyled.io>
2020-10-28 09:13:34 +0000

    [Restyler] shfmt

--- apps/ci/ci-os-config-core.sh
+++ apps/ci/ci-os-config-core.sh
@@ -11,15 +11,15 @@ time sudo apt-get install -y ccache cmake make gcc g++ clang \
   libboost-system1.6*-dev libboost-filesystem1.6*-dev libboost-program-options1.6*-dev libboost-iostreams1.6*-dev
 
 case $COMPILER in
-  "clang7" )
+  "clang7")
     time sudo apt-get install -y clang-7
     ;;
 
-  "clang10" )
+  "clang10")
     time sudo apt-get install -y clang-10
     ;;
 
-  * )
+  *)
     echo "Unknown compiler $COMPILER"
     exit 1
     ;;
