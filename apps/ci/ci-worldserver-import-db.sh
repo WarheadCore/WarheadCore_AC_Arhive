23e923429e3251bc187ce0acd4efb83cec8c9bb1
Restyled.io <commits@restyled.io>
2020-10-28 18:41:33 +0000

    [Restyler] shfmt

--- apps/ci/ci-worldserver-import-db.sh
+++ apps/ci/ci-worldserver-import-db.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-echo "[worldserver]" >> ./build/check_install/etc/worldserver.conf
-echo "DataDir = \"../data/\"" >> ./build/check_install/etc/worldserver.conf
-echo "LoginDatabaseInfo     = \"localhost;3306;root;root;acore_auth\"" >> ./build/check_install/etc/worldserver.conf
-echo "WorldDatabaseInfo     = \"localhost;3306;root;root;acore_world\"" >> ./build/check_install/etc/worldserver.conf
-echo "CharacterDatabaseInfo = \"localhost;3306;root;root;acore_characters\"" >> ./build/check_install/etc/worldserver.conf
+echo "[worldserver]" >>./build/check_install/etc/worldserver.conf
+echo "DataDir = \"../data/\"" >>./build/check_install/etc/worldserver.conf
+echo "LoginDatabaseInfo     = \"localhost;3306;root;root;acore_auth\"" >>./build/check_install/etc/worldserver.conf
+echo "WorldDatabaseInfo     = \"localhost;3306;root;root;acore_world\"" >>./build/check_install/etc/worldserver.conf
+echo "CharacterDatabaseInfo = \"localhost;3306;root;root;acore_characters\"" >>./build/check_install/etc/worldserver.conf
 git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git ./build/check_install/data
 (cd ./build/check_install/bin/ && timeout 5m ./worldserver --import-db)
