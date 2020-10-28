7f3e9e124fa39dde2280940e87994c1caee9b3fe
Winfidonarleyan <dowlandtop@yandex.com>
2020-10-29 01:38:47 +0700

    10

--- apps/ci/ci-worldserver-import-db.sh
+++ apps/ci/ci-worldserver-import-db.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-echo "[worldserver]" >> ./env/dist/etc/worldserver.conf
-echo "DataDir = \"../data/\"" >> ./env/dist/etc/worldserver.conf
-echo "LoginDatabaseInfo     = \"localhost;3306;root;root;acore_auth\"" >> ./env/dist/etc/worldserver.conf
-echo "WorldDatabaseInfo     = \"localhost;3306;root;root;acore_world\"" >> ./env/dist/etc/worldserver.conf
-echo "CharacterDatabaseInfo = \"localhost;3306;root;root;acore_characters\"" >> ./env/dist/etc/worldserver.conf
-git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git ./env/dist/data
-(cd ./env/dist/bin/ && timeout 5m ./worldserver --import-db)
+echo "[worldserver]" >> ./build/check_install/etc/worldserver.conf
+echo "DataDir = \"../data/\"" >> ./build/check_install/etc/worldserver.conf
+echo "LoginDatabaseInfo     = \"localhost;3306;root;root;acore_auth\"" >> ./build/check_install/etc/worldserver.conf
+echo "WorldDatabaseInfo     = \"localhost;3306;root;root;acore_world\"" >> ./build/check_install/etc/worldserver.conf
+echo "CharacterDatabaseInfo = \"localhost;3306;root;root;acore_characters\"" >> ./build/check_install/etc/worldserver.conf
+git clone --depth=1 --branch=master --single-branch https://github.com/ac-data/ac-data.git ./build/check_install/data
+(cd ./build/check_install/bin/ && timeout 5m ./worldserver --import-db)
