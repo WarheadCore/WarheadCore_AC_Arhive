7a7ec209e95cbc25babb3d79cb4fde021d1f7587
Restyled.io <commits@restyled.io>
2020-10-29 05:55:32 +0000

    [Restyler] shfmt

--- apps/ci/ci-codestyle.sh
+++ apps/ci/ci-codestyle.sh
@@ -5,18 +5,18 @@ echo "Codestyle check script:"
 echo
 
 declare -A singleLineRegexChecks=(
-    ["[[:blank:]]$"]="Remove whitespace at the end of the lines above"
-    ["\t"]="Replace tabs with 4 spaces in the lines above"
+  ["[[:blank:]]$"]="Remove whitespace at the end of the lines above"
+  ["\t"]="Replace tabs with 4 spaces in the lines above"
 )
 
 for check in ${!singleLineRegexChecks[@]}; do
-    echo "  Checking RegEx: '$check'"
-    
-    if grep -P -r -I -n "$check" src; then
-        echo
-        echo "${singleLineRegexChecks[$check]}"
-        exit 1
-    fi
+  echo "  Checking RegEx: '$check'"
+
+  if grep -P -r -I -n "$check" src; then
+    echo
+    echo "${singleLineRegexChecks[$check]}"
+    exit 1
+  fi
 done
 
 echo
