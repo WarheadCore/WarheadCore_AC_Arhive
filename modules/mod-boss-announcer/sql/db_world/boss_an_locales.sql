99d05b792463e73f93280da71523f550b47e2024
Winfidonarleyan <dowlandtop@yandex.com>
2020-11-06 10:01:35 +0700

    restyle message

--- modules/mod-boss-announcer/sql/db_world/boss_an_locales.sql
+++ modules/mod-boss-announcer/sql/db_world/boss_an_locales.sql
@@ -4,11 +4,11 @@
 
 DELETE FROM `string_module`
 WHERE `ModuleName` = 'mod-boss-announcer'
-    AND `ID` BETWEEN 1 AND 40;
+    AND `ID` BETWEEN 1 AND 5;
 
 INSERT INTO `string_module` (`ModuleName`, `ID`, `Locale`, `Text`)
         VALUES
             -- ruRU
-            ('mod-boss-announcer', 1, 'ruRU', '|cffff0000# |cff7bbef7%s|r и его группа убили босса |cffff0000%s|r |cff7bbef7в режиме|r %s'), ('mod-boss-announcer', 2, 'ruRU', '10 об.'), ('mod-boss-announcer', 3, 'ruRU', '10 гер.'), ('mod-boss-announcer', 4, 'ruRU', '25 об.'), ('mod-boss-announcer', 5, 'ruRU', '25 гер.'),
+            ('mod-boss-announcer', 1, 'ruRU', '|cffff0000# |cff7bbef7%s|r и его группа убили босса |cffff0000%s|r в режиме %s'), ('mod-boss-announcer', 2, 'ruRU', '10 об.'), ('mod-boss-announcer', 3, 'ruRU', '10 гер.'), ('mod-boss-announcer', 4, 'ruRU', '25 об.'), ('mod-boss-announcer', 5, 'ruRU', '25 гер.'),
             -- enUS
-            ('mod-boss-announcer', 1, 'enUS', '|cffff0000# |cff7bbef7%s|r and his group killed boss |cffff0000%s|r |cff7bbef7in difficulty|r %s'), ('mod-boss-announcer', 2, 'enUS', '10 def.'), ('mod-boss-announcer', 3, 'enUS', '10 her.'), ('mod-boss-announcer', 4, 'enUS', '25 def.'), ('mod-boss-announcer', 5, 'enUS', '25 her.');
+            ('mod-boss-announcer', 1, 'enUS', '|cffff0000# |cff7bbef7%s|r and his group killed boss |cffff0000%s|r in difficulty %s'), ('mod-boss-announcer', 2, 'enUS', '10 def.'), ('mod-boss-announcer', 3, 'enUS', '10 her.'), ('mod-boss-announcer', 4, 'enUS', '25 def.'), ('mod-boss-announcer', 5, 'enUS', '25 her.');
