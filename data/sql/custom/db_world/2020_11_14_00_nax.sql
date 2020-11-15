e707e5a4d5eef31f10aecb79cb06e6734bc4c354
Restyled.io <commits@restyled.io>
2020-11-15 15:02:02 +0000

    [Restyler] pg_format

--- data/sql/custom/db_world/2020_11_14_00_nax.sql
+++ data/sql/custom/db_world/2020_11_14_00_nax.sql
@@ -1,4 +1,7 @@
 -- Add Lich king to all difficulties
-DELETE FROM `creature` WHERE (`id` = 16980) AND (`guid` IN (1971311));
-INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES
-(1971311, 16980, 533, 0, 0, 15, 1, 0, 0, 3749.68, -5114.06, 142.115, 2.93215, 604800, 0, 0, 1, 0, 0, 0, 0, 0, '', 0);
+DELETE FROM `creature`
+WHERE (`id` = 16980)
+    AND (`guid` IN (1971311));
+
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `wander_distance`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `ScriptName`, `VerifiedBuild`)
+        VALUES (1971311, 16980, 533, 0, 0, 15, 1, 0, 0, 3749.68, - 5114.06, 142.115, 2.93215, 604800, 0, 0, 1, 0, 0, 0, 0, 0, '', 0);
