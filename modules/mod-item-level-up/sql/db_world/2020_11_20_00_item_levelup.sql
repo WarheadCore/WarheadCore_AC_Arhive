5c1eb6d7b398abbe7a11b8c4e7e530503ae29437
Restyled.io <commits@restyled.io>
2020-11-19 23:58:18 +0000

    [Restyler] pg_format

--- modules/mod-item-level-up/sql/db_world/2020_11_20_00_item_levelup.sql
+++ modules/mod-item-level-up/sql/db_world/2020_11_20_00_item_levelup.sql
@@ -12,13 +12,13 @@ WHERE `ModuleName` = @MODULE_NAME
 INSERT INTO `string_module` (`ModuleName`, `ID`, `Locale`, `Text`)
         VALUES
             -- ruRU
-            (@MODULE_NAME, 1, 'ruRU', '|cffff0000# |cff00ff00Вы уже имеете максимально возможный уровень в игре:|r %u'),
-            (@MODULE_NAME, 2, 'ruRU', '|cffff0000# |cff00ff00Вы получили максимальный уровень в игре:|r %u'),
+            (@MODULE_NAME, 1, 'ruRU', '|cffff0000# |cff00ff00Вы уже имеете максимально возможный уровень в игре:|r %u'), (@MODULE_NAME, 2, 'ruRU', '|cffff0000# |cff00ff00Вы получили максимальный уровень в игре:|r %u'),
             -- enUS
-            (@MODULE_NAME, 1, 'enUS', '|cffff0000# |cff00ff00You already have the highest possible level in the game:|r %s'),
-            (@MODULE_NAME, 2, 'enUS', '|cffff0000# |cff00ff00You got the maximum level in game:|r %s');
-
+            (@MODULE_NAME, 1, 'enUS', '|cffff0000# |cff00ff00You already have the highest possible level in the game:|r %s'), (@MODULE_NAME, 2, 'enUS', '|cffff0000# |cff00ff00You got the maximum level in game:|r %s');
 -- Add item
-DELETE FROM `item_template` WHERE `entry` = 90000;
-INSERT INTO `item_template` VALUES
-(90000, 0, 0, -1, 'Level-Up Token', 46787, 6, 0, 0, 1, 0, 0, 0, -1, -1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1000, 0, 0, 18282, 0, 0, 0, 8000, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, 0, 0, 0, -1, 0, -1, 0, '|cff00FF00Use: Instantly levels your character to the next level.|r', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 'ItemLevelUp_Item', 0, 0, 0, 0, 0, -4);
+
+DELETE FROM `item_template`
+WHERE `entry` = 90000;
+
+INSERT INTO `item_template`
+        VALUES (90000, 0, 0, - 1, 'Level-Up Token', 46787, 6, 0, 0, 1, 0, 0, 0, - 1, - 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1000, 0, 0, 18282, 0, 0, 0, 8000, 0, - 1, 0, 0, 0, 0, - 1, 0, - 1, 0, 0, 0, 0, - 1, 0, - 1, 0, 0, 0, 0, - 1, 0, - 1, 0, 0, 0, 0, - 1, 0, - 1, 0, '|cff00FF00Use: Instantly levels your character to the next level.|r', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, - 1, 0, 0, 0, 0, 'ItemLevelUp_Item', 0, 0, 0, 0, 0, - 4);
