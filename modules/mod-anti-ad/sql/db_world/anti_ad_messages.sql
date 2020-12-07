6da6b737ab29e30dc92619abc300e9b8e7fef88f
Winfidonarleyan <dowlandtop@yandex.com>
2020-12-07 14:00:54 +0700

    feat(Module/AntiAD): rework check message systme

--- modules/mod-anti-ad/sql/db_world/anti_ad_messages.sql
+++ modules/mod-anti-ad/sql/db_world/anti_ad_messages.sql
@@ -2,37 +2,36 @@
 -- Table structure for anti_ad_messages
 -- ----------------------------
 DROP TABLE IF EXISTS `anti_ad_messages`;
-CREATE TABLE `anti_ad_messages`  (
-  `message` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`message`) USING BTREE
+
+-- ----------------------------
+-- Table structure for anti_ad_patterns
+-- ----------------------------
+DROP TABLE IF EXISTS `anti_ad_patterns`;
+CREATE TABLE `anti_ad_patterns`  (
+  `Pattern` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`Pattern`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Records of anti_ad_messages
+-- Records of anti_ad_patterns
 -- ----------------------------
-INSERT INTO `anti_ad_messages` VALUES ('(w)(o)(w)');
-INSERT INTO `anti_ad_messages` VALUES ('.com');
-INSERT INTO `anti_ad_messages` VALUES ('.eu');
-INSERT INTO `anti_ad_messages` VALUES ('.fr');
-INSERT INTO `anti_ad_messages` VALUES ('.info');
-INSERT INTO `anti_ad_messages` VALUES ('.net');
-INSERT INTO `anti_ad_messages` VALUES ('.org');
-INSERT INTO `anti_ad_messages` VALUES ('.ru');
-INSERT INTO `anti_ad_messages` VALUES ('.zapto');
-INSERT INTO `anti_ad_messages` VALUES ('.рф');
-INSERT INTO `anti_ad_messages` VALUES ('://');
-INSERT INTO `anti_ad_messages` VALUES ('http');
-INSERT INTO `anti_ad_messages` VALUES ('no-ip');
-INSERT INTO `anti_ad_messages` VALUES ('w-o-w');
-INSERT INTO `anti_ad_messages` VALUES ('wow');
-INSERT INTO `anti_ad_messages` VALUES ('www');
+INSERT INTO `anti_ad_patterns`(`Pattern`) VALUES 
+('?:[-a-z0-9@:%_\\+~.#=]{2,256}\\.)?([-a-z0-9@:%_\\+~#=]*)\\.[a-z]{2,6}\\b(?:[-a-z0-9@:%_\\+.~#?&\\/\\/=]*'),
+('м\\s*а\\s*м\\s*у'),
+('м\\s*а\\s*т\\s*ь'),
+('w\\s*o\\s*w');
+
+-- Set module name
+SET @MODULE_NAME := 'mod-anti-ad';
 
 -- ----------------------------
 -- Records of string_module
 -- ----------------------------
-INSERT INTO `string_module` VALUES ('mod-anti-ad', 1, 'enUS', '|cFFFF0000[AntiAD]:|r %s |cff6C8CD5said a bad word:|r %s');
-INSERT INTO `string_module` VALUES ('mod-anti-ad', 2, 'enUS', '|cFFFF0000[AntiAD]:|r %s |cff6C8CD5wanted to say:|r %s');
-INSERT INTO `string_module` VALUES ('mod-anti-ad', 3, 'enUS', '|cFFFF0000[AntiAD]:|cff6C8CD5 You chat muted on |r %u |cff6C8CD5minutes.');
-INSERT INTO `string_module` VALUES ('mod-anti-ad', 1, 'ruRU', '|cFFFF0000[Антиреклама]:|r %s |cff6C8CD5сказал запрещённое слово:|r %s');
-INSERT INTO `string_module` VALUES ('mod-anti-ad', 2, 'ruRU', '|cFFFF0000[Антиреклама]:|r %s |cff6C8CD5хотел сказать:|r %s');
-INSERT INTO `string_module` VALUES ('mod-anti-ad', 3, 'ruRU', '|cFFFF0000[Антиреклама]:|cff6C8CD5 Ваш чат заблокирован на|r %u |cff6C8CD5минут.');
+DELETE FROM `string_module`
+WHERE `ModuleName` = @MODULE_NAME;
+
+INSERT INTO `string_module` VALUES 
+(@MODULE_NAME, 1, 'enUS', '|cFFFF0000[AntiAD]:|r %s |cff6C8CD5wanted to say:|r %s'),
+(@MODULE_NAME, 2, 'enUS', '|cFFFF0000[AntiAD]:|cff6C8CD5 You chat muted on |r %u |cff6C8CD5minutes.'),
+(@MODULE_NAME, 1, 'ruRU', '|cFFFF0000[Антиреклама]:|r %s |cff6C8CD5хотел сказать:|r %s'),
+(@MODULE_NAME, 2, 'ruRU', '|cFFFF0000[Антиреклама]:|cff6C8CD5 Ваш чат заблокирован на|r %u |cff6C8CD5минут.');
