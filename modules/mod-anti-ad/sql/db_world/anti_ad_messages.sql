21fbd3675862c1da2efcb029cfd9afddc1dbdad1
Restyled.io <commits@restyled.io>
2020-12-07 07:03:55 +0000

    [Restyler] pg_format

--- modules/mod-anti-ad/sql/db_world/anti_ad_messages.sql
+++ modules/mod-anti-ad/sql/db_world/anti_ad_messages.sql
@@ -1,37 +1,38 @@
 -- ----------------------------
 -- Table structure for anti_ad_messages
 -- ----------------------------
+
 DROP TABLE IF EXISTS `anti_ad_messages`;
 
 -- ----------------------------
 -- Table structure for anti_ad_patterns
 -- ----------------------------
+
 DROP TABLE IF EXISTS `anti_ad_patterns`;
-CREATE TABLE `anti_ad_patterns`  (
-  `Pattern` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`Pattern`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+CREATE TABLE `anti_ad_patterns` (
+    `Pattern` varchar(255) CHARACTER
+    SET utf8 COLLATE utf8_general_ci NOT NULL,
+    PRIMARY KEY (`Pattern`)
+    USING BTREE) ENGINE = InnoDB CHARACTER
+SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of anti_ad_patterns
 -- ----------------------------
-INSERT INTO `anti_ad_patterns`(`Pattern`) VALUES 
-('?:[-a-z0-9@:%_\\+~.#=]{2,256}\\.)?([-a-z0-9@:%_\\+~#=]*)\\.[a-z]{2,6}\\b(?:[-a-z0-9@:%_\\+.~#?&\\/\\/=]*'),
-('м\\s*а\\s*м\\s*у'),
-('м\\s*а\\s*т\\s*ь'),
-('w\\s*o\\s*w');
 
+INSERT INTO `anti_ad_patterns` (`Pattern`)
+        VALUES ('?:[-a-z0-9@:%_\\+~.#=]{2,256}\\.)?([-a-z0-9@:%_\\+~#=]*)\\.[a-z]{2,6}\\b(?:[-a-z0-9@:%_\\+.~#?&\\/\\/=]*'), ('м\\s*а\\s*м\\s*у'), ('м\\s*а\\s*т\\s*ь'), ('w\\s*o\\s*w');
 -- Set module name
+
 SET @MODULE_NAME := 'mod-anti-ad';
 
 -- ----------------------------
 -- Records of string_module
 -- ----------------------------
+
 DELETE FROM `string_module`
 WHERE `ModuleName` = @MODULE_NAME;
 
-INSERT INTO `string_module` VALUES 
-(@MODULE_NAME, 1, 'enUS', '|cFFFF0000[AntiAD]:|r %s |cff6C8CD5wanted to say:|r %s'),
-(@MODULE_NAME, 2, 'enUS', '|cFFFF0000[AntiAD]:|cff6C8CD5 You chat muted on |r %u |cff6C8CD5minutes.'),
-(@MODULE_NAME, 1, 'ruRU', '|cFFFF0000[Антиреклама]:|r %s |cff6C8CD5хотел сказать:|r %s'),
-(@MODULE_NAME, 2, 'ruRU', '|cFFFF0000[Антиреклама]:|cff6C8CD5 Ваш чат заблокирован на|r %u |cff6C8CD5минут.');
+INSERT INTO `string_module`
+        VALUES (@MODULE_NAME, 1, 'enUS', '|cFFFF0000[AntiAD]:|r %s |cff6C8CD5wanted to say:|r %s'), (@MODULE_NAME, 2, 'enUS', '|cFFFF0000[AntiAD]:|cff6C8CD5 You chat muted on |r %u |cff6C8CD5minutes.'), (@MODULE_NAME, 1, 'ruRU', '|cFFFF0000[Антиреклама]:|r %s |cff6C8CD5хотел сказать:|r %s'), (@MODULE_NAME, 2, 'ruRU', '|cFFFF0000[Антиреклама]:|cff6C8CD5 Ваш чат заблокирован на|r %u |cff6C8CD5минут.');
