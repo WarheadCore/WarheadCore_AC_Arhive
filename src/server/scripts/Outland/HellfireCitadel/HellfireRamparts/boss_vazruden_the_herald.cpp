4fb576acaec72c57942783f08a2b9398c329b78d
Viste <viste02@gmail.com>
2021-01-11 23:05:02 +0300

    Core/Scritps: fix crash in spell call nazan

--- src/server/scripts/Outland/HellfireCitadel/HellfireRamparts/boss_vazruden_the_herald.cpp
+++ src/server/scripts/Outland/HellfireCitadel/HellfireRamparts/boss_vazruden_the_herald.cpp
@@ -363,7 +363,10 @@ public:
         void HandleScriptEffect(SpellEffIndex /*effIndex*/)
         {
             if (Unit* target = GetHitUnit())
-                target->GetAI()->DoAction(ACTION_FLY_DOWN);
+                if (!target)
+                    return;
+                else
+                    target->GetAI()->DoAction(ACTION_FLY_DOWN);
         }
 
         void Register()
